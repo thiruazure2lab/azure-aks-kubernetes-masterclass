0f1a3663a76cd1748853ea52ed7a618817eba179
thiruazure2lab <thiruazure2lab@gmail.com>
2024-07-03 19:18:03 +0530

    System node pool must use VM sku with more than 2 cores and 4GB memory. Nodepool name: systempool.

--- 24-Azure-AKS-Terraform/24-04-Create-AKS-NodePools-using-Terraform/terraform-manifests-aks/07-aks-cluster.tf
+++ 24-Azure-AKS-Terraform/24-04-Create-AKS-NodePools-using-Terraform/terraform-manifests-aks/07-aks-cluster.tf
@@ -34,7 +34,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
   default_node_pool {
     name                 = "systempool"
-    vm_size              = "Standard_D1_v2"
+    vm_size              = "Standard_DS2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     #availability_zones   = [1, 2, 3]
     # Added June2023
