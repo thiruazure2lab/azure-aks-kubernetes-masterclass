ad97cfca795d0c2fe2422a7d46aee8367f8f6b88
thiruazure2lab <thiruazure2lab@gmail.com>
2024-07-03 17:38:16 +0530

    replace stg acc with terraformstate2024thiru

--- 24-Azure-AKS-Terraform/24-05-Create-AKS-Cluster-Custom-VNET/terraform-manifests-aks-custom-vnet/01-main.tf
+++ 24-Azure-AKS-Terraform/24-05-Create-AKS-Cluster-Custom-VNET/terraform-manifests-aks-custom-vnet/01-main.tf
@@ -29,7 +29,7 @@ terraform {
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name   = "terraform-storage-rg"
-    storage_account_name  = "terraformstatekalyan"
+    storage_account_name  = "terraformstate2024thiru"
     container_name        = "tfstatefiles"
     key                   = "terraform-custom-vnet.tfstate"
   }  
